fd61d408ee78bcc40eabba10d0a0a0977b0f7391
SkyLiNing823 <84125881+SkyLiNing823@users.noreply.github.com>
2025-05-26 16:28:24 +0800

    alembic added

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:910823@localhost/TodoApplicationDatabase'
-
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:910823@localhost/TodoApplicationDatabase'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
